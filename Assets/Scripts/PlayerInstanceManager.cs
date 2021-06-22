c43c0829e396baf2bc71256b12b23e0ad0568b92
Conrad <conradjwilson@hotmail.com>
2021-06-22 14:00:20 +0100

    Multiple inputs & Spawning Individual Players

---
+++ Assets/Scripts/PlayerInstanceManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInstanceManager : MonoBehaviour
+{
+    public static PlayerInstanceManager instance { get; private set; }
+
+    public List<GameObject> players;
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
