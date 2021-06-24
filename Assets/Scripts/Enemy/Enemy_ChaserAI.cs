dedd39adb7ab2de1b0682c2a47156d87192647af
Kieran-De-Sousa <kieran2.desousa@live.uwe.ac.uk>
2021-06-24 14:05:57 +0100

    Player Damaging

--- Assets/Scripts/Enemy/Enemy_ChaserAI.cs
+++ Assets/Scripts/Enemy/Enemy_ChaserAI.cs
@@ -32,6 +32,7 @@ public class Enemy_ChaserAI : MonoBehaviour
         enemyPos = transform.position;
 
         player1 = GameObject.FindGameObjectWithTag("Brains");
+<<<<<<< Updated upstream
         player2 = GameObject.FindGameObjectWithTag("Brawn");
 
     }
@@ -78,6 +79,65 @@ public class Enemy_ChaserAI : MonoBehaviour
     private void attackPlayer(target currentTarget)
     {
         //Enemy stops moving
+=======
+        player2 = GameObject.FindGameObjectWithTag("Brawn");
+
+    }
+
+    void Update()
+    {
+        if (player1 == null)
+        {
+            player1 = GameObject.FindGameObjectWithTag("Brains");
+        }
+        if (player2 == null)
+        {
+            player2 = GameObject.FindGameObjectWithTag("Brawn");
+        }
+        if (player1 != null && player2 != null)
+        {
+            //Targets player 1 - Distance to player 1 is smaller
+            if (Vector3.Distance(enemyPos, player1.transform.position) < Vector3.Distance(enemyPos, player2.transform.position))
+            {
+                currentTarget = target.PLAYER1;
+            }
+            //Targets player 2 - Distance to player 2 is smaller
+            else if (Vector3.Distance(enemyPos, player2.transform.position) < Vector3.Distance(enemyPos, player1.transform.position))
+            {
+                currentTarget = target.PLAYER2;
+            }
+            //Distances between 2 players and enemy are equal
+            else
+            {
+                currentTarget = target.PLAYER1;
+            }
+
+            playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, playerLayer);
+
+            //Function calls for enemy state
+            chasePlayer(currentTarget);
+            if (playerInAttackRange)
+            {
+                attackPlayer(currentTarget);
+            } 
+        }
+    }
+
+    private void chasePlayer(target currentTarget)
+    {
+        if (currentTarget == target.PLAYER1)
+        {
+            agent.SetDestination(player1.transform.position);
+        }
+        else if (currentTarget == target.PLAYER2)
+        {
+            agent.SetDestination(player2.transform.position);
+        }
+    }
+    private void attackPlayer(target currentTarget)
+    {
+        //Enemy stops moving
+>>>>>>> Stashed changes
         agent.SetDestination(transform.position);
 
         if (currentTarget == target.PLAYER1)
@@ -95,10 +155,15 @@ public class Enemy_ChaserAI : MonoBehaviour
             if (currentTarget == target.PLAYER1)
             {
                 Debug.Log("Attacking Player 1");
+                GameObject HUD = GameObject.Find("PlayerHUDPrefab");
+                HUD.GetComponent<PlayerHUDController>().DealDamage(1, 2.0f);
             }
             else if (currentTarget == target.PLAYER2)
             {
                 Debug.Log("Attacking Player 2");
+                Debug.Log("Attacking Player 1");
+                GameObject HUD = GameObject.Find("PlayerHUDPrefab");
+                HUD.GetComponent<PlayerHUDController>().DealDamage(2, 2.0f);
             }
             //
             hasAttacked = true;
