52d7fdbdd0ad89f8823da6c9b3ef62730662a4b3
Kye Dudley <kye2.dudley@live.uwe.ac.uk>
2021-06-24 16:54:10 +0100

    Player Select Screen

---
+++ Assets/UI/MainMenu/SliderPlayScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderPlayScript : MonoBehaviour
+{
+    public Button PlayButton;
+    public Slider P1Slider;
+    public Slider P2Slider;
+    private bool canPlay = true;
+    void Update()
+    {
+        if (P1Slider.value == P2Slider.value)
+        {
+            canPlay = false;
+            PlayButton.enabled = false;
+            Debug.Log("Cannot start. Players must be different characters");
+        }
+        else
+        {
+            canPlay = true;
+            PlayButton.enabled = true;
+            Debug.Log("Can Begin.");
+        }
+    }
+}
