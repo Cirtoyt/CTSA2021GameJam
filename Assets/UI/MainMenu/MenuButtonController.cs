776a1f31eaac1a926efefa5bcc877ac1e8cb027b
Kye Dudley <kye2.dudley@live.uwe.ac.uk>
2021-06-24 13:10:07 +0100

    UI transitions script
    you can select different options in the inspector.

---
+++ Assets/UI/MainMenu/MenuButtonController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonController : MonoBehaviour
+{
+    public enum onCLick {None,Transition,CloseGame,OpenScene};
+    public onCLick clickResult;
+
+    public string SceneName;
+    public GameObject CurrentScreen;
+    public GameObject NextScreen;
+
+    private void Start()
+    {
+        if(clickResult == onCLick.Transition)
+        {
+            gameObject.GetComponent<Button>().onClick.AddListener(TransitionPress);
+        }
+        else if (clickResult == onCLick.CloseGame)
+        {
+            gameObject.GetComponent<Button>().onClick.AddListener(CloseGamePress);
+        }
+        else if (clickResult == onCLick.OpenScene)
+        {
+            gameObject.GetComponent<Button>().onClick.AddListener(OpenScenePress);
+        }
+    }
+    public void TransitionPress()
+    {
+        CurrentScreen.SetActive(false);
+        NextScreen.SetActive(true);
+    }
+    public void CloseGamePress()
+    {
+        Application.Quit();
+    }
+    public void OpenScenePress()
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
