adc8a1ad1401c7d28270fc2dcd97e8cc25d76d9a
Conrad <conradjwilson@hotmail.com>
2021-06-25 14:01:56 +0100

    Brain solo ult DONE

--- Assets/Scripts/GravityBomb.cs
+++ Assets/Scripts/GravityBomb.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GravityBomb : MonoBehaviour
 {
     [SerializeField] private float duration;
     [SerializeField] private float throwHeightAngle;
     [SerializeField] private float throwStrength;
+    [SerializeField] private LayerMask enemyLayer;
+    [SerializeField] private float pullForce;
 
     private float elapsed;
+    [SerializeField] private List<Transform> enemiesEffected = new List<Transform>();
 
     void Start()
     {
@@ -30,9 +34,36 @@ public class GravityBomb : MonoBehaviour
 
         if (elapsed >= duration)
         {
+            foreach (Transform enemy in enemiesEffected)
+            {
+                enemy.GetComponent<NavMeshAgent>().isStopped = false;
+                enemy.GetComponent<Rigidbody>().isKinematic = true;
+            }
+
             Destroy(gameObject);
         }
 
         // Find enemies within 3 radius and suck them inwords
+        foreach (Collider enemy in Physics.OverlapSphere(transform.position, 3, enemyLayer))
+        {
+            if (!enemiesEffected.Contains(enemy.transform))
+            {
+                enemiesEffected.Add(enemy.transform);
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        foreach (Transform enemy in enemiesEffected)
+        {
+            enemy.GetComponent<NavMeshAgent>().isStopped = true;
+            enemy.GetComponent<Rigidbody>().isKinematic = false;
+
+            Vector3 pullInDirection = (transform.position - enemy.transform.position).normalized;
+            float gravityMultiplier = (2.9f - Vector3.Distance(enemy.transform.position, transform.position))
+                                    * (2.9f - Vector3.Distance(enemy.transform.position, transform.position));
+            enemy.GetComponent<Rigidbody>().AddForce(pullInDirection * pullForce * gravityMultiplier);
+        }
     }
 }
