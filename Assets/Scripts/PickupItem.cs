b53b20241a83307c7dd16ad2419765bfc27206e3
Christy Dwyer <christy2.dwyer@live.uwe.ac.uk>
2021-06-25 15:29:16 +0100

    Adding laser to brains

--- Assets/Scripts/PickupItem.cs
+++ Assets/Scripts/PickupItem.cs
@@ -5,18 +5,29 @@ using UnityEngine;
 public class PickupItem : MonoBehaviour
 {
     private GameObject objectiveManagerObject;
+    private bool canPickup;
 
     void Start()
     {
         objectiveManagerObject = GameObject.Find("Objective Manager");
+        canPickup = false;
+
+        StartCoroutine(enablePickup());
+
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.layer == 10)
+        if (canPickup && collision.gameObject.layer == 10)
         {
             objectiveManagerObject.GetComponent<ObjectiveManager>().incrementItems();
             Destroy(gameObject);
         }
+    }
+
+    private IEnumerator enablePickup()
+    {
+        yield return new WaitForSeconds(1);
+        canPickup = true;
     }
 }
