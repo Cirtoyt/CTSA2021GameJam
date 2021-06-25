40be2cf9f3cc9e9a908dd08c0519c4102a7533f0
Conrad <conradjwilson@hotmail.com>
2021-06-25 15:26:22 +0100

    Is this the one? O_o

---
+++ Assets/UI/MainMenu/SliderPlayScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderPlayScript : MonoBehaviour
+{
+    public Button PlayButton;
+    public Text PlayButtonText;
+    public Slider P1Slider;
+    public Slider P2Slider;
+    void Update()
+    {
+        if (P1Slider.value == P2Slider.value)
+        {
+            PlayButton.enabled = false;
+            PlayButton.GetComponent<Image>().color = Color.gray;
+            PlayButtonText.GetComponent<Text>().enabled = true;
+        }
+        else
+        {
+            PlayButton.enabled = true;
+            PlayButton.GetComponent<Image>().color = Color.white;
+            PlayButtonText.GetComponent<Text>().enabled = false;
+        }
+    }
+}
