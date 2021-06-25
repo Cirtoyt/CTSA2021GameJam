60860583ce0107fa79759cff3f9d8a62eb2bfb89
Marcus A Ochai <ochaimarcus9@gmail.com>
2021-06-25 12:23:53 +0100

    Kieran - Hacking monitors started

---
+++ Assets/HackingMonitor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scri : MonoBehaviour
+{
+    private GameObject brains;
+    private bool isHacking;
+    // Start is called before the first frame update
+    void Start()
+    {
+        brains = GameObject.FindGameObjectWithTag("Brains");
+        isHacking = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
